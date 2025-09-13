2e45fc8ef01cf4741686ce4437d7e2ce8e727c42
awewsomegamer <awewsomegamer@gmail.com>
2025-09-12 23:50:25 -0500

    Add profiling interface

---
+++ src/c/include/mp/profiling.h
@@ -0,0 +1,51 @@
+/**
+ * @file profiling.h
+ *
+ * @author awewsomegamer <awewsomegamer@gmail.com>
+ *
+ * @LICENSE
+ * Arctan-OS/Kernel - Operating System Kernel
+ * Copyright (C) 2023-2025 awewsomegamer
+ *
+ * This file is part of Arctan-OS/Kernel.
+ *
+ * Arctan is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @DESCRIPTION
+*/
+#ifndef ARC_MP_PROFILING_H
+#define ARC_MP_PROFILING_H
+
+#include <stdint.h>
+
+enum {
+	ARC_THREAD_PROFILE_MEM = 0,
+	ARC_THREAD_PROFILE_IO,
+	ARC_THREAD_PROFILE_COUNT
+};
+
+typedef struct ARC_Profile {
+	struct {
+		uint64_t io_requests;
+		uint64_t suspensions;
+		// TODO: MORE DATA POINTS!
+	} data;
+	uint32_t type; // Cached type
+} ARC_Profile;
+
+// All sorts of hook functions
+
+uint32_t profile(ARC_Profile *profile);
+
+#endif
